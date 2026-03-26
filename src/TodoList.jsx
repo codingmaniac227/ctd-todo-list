7dbc1c76f436ba74184ef40331eb5e7b09b52511
Coding <codingmaniac227@gmail.com>
2026-03-26 00:15:42 -0400

    refactor(TodoList): mapped state-managed todos render through component

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -1,16 +1,13 @@
-function TodoList() {
-    const todoList = [
-        {id: 1, title: "review resources"},
-        {id: 2, title: "take notes"},
-        {id: 3, title: "code out app"}
-    ]
+import TodoListItem from "./TodoListItem.jsx";
+
+function TodoList({todoList}) {
 
     return (
         <>
             <div>
                 <h2>My Todos</h2>
                 <ul>
-                    {todoList.map(todo => <li key={todo.id}>{todo.title}</li>)}
+                    {todoList.map(todo => <TodoListItem todo={todo}/>)}
                 </ul>
             </div>
         </>
