db3cd47b64f8b311be1edbfcbda332762cd3a7d2
Coding <codingmaniac227@gmail.com>
2026-03-27 02:58:27 -0400

    refactor(TodoList): todo list now served by a filtered list of non-completed todos

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -1,17 +1,21 @@
 import TodoListItem from "./TodoListItem.jsx";
 
-function TodoList({todoList}) {
+function TodoList({todoList, onCompleteTodo}) {
 
-    return (
-        <>
-            <div>
-                <h2>My Todos</h2>
-                <ul>
-                    {todoList.map(todo => <TodoListItem key={todo.id} todo={todo}/>)}
-                </ul>
-            </div>
-        </>
-    )
+    const filteredTodoList = todoList.filter(todo => todo.isCompleted === false)
+
+    return filteredTodoList.length === 0 ? (
+        <p>Add todo above to get started</p>
+        ) : (
+            <>
+                <div>
+                    <h2>My Todos</h2>
+                    <ul>
+                        {filteredTodoList.map(todo => <TodoListItem key={todo.id} todo={todo} onCompleteTodo={onCompleteTodo}/>)}
+                    </ul>
+                </div>
+            </>
+        )
 }
 
 export default TodoList
