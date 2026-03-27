7adc9bf38c48cd73faeee047c974bdd74f578841
Coding <codingmaniac227@gmail.com>
2026-03-27 02:49:28 -0400

    refactor(TodoForm): local state-controlled input implemented

--- src/TodoForm.jsx
+++ src/TodoForm.jsx
@@ -1,15 +1,15 @@
-import { useRef } from "react";
+import { useRef, useState } from "react";
 
 function TodoForm({ onAddTodo }) {
     const inputRef = useRef(null)
 
+    const [ workingTodoTitle, setWorkingTodoTitle ] = useState("")
+
     const handleAddTodo = (event) => {
         event.preventDefault()
-
-        const todoTitle = event.target.todoTitle.value.trim()
-        if (todoTitle && todoTitle !== "") {
-            onAddTodo(todoTitle)
-            event.target.reset()
+        if (workingTodoTitle && workingTodoTitle !== "") {
+            onAddTodo(workingTodoTitle)
+            setWorkingTodoTitle("")
             inputRef.current.focus()
         }
     }
@@ -24,9 +24,11 @@ function TodoForm({ onAddTodo }) {
                     id="todoTitle"
                     name="todoTitle"
                     placeholder={'Todo text'}
+                    value={workingTodoTitle}
+                    onChange={(event) => setWorkingTodoTitle(event.target.value)}
                     required
                 />
-                <button type="submit">Add Todo</button>
+                <button type="submit" disabled={!workingTodoTitle.trim()}>Add Todo</button>
             </form>
         </>
     )
