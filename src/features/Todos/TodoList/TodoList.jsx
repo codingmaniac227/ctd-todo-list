129b697b02cd433b3f3c82b40f68b3e210f65118
Coding <codingmaniac227@gmail.com>
2026-04-23 19:09:47 -0400

    refactor(TodoList): move component to dedicated folder for separation of concerns

---
+++ src/features/Todos/TodoList/TodoList.jsx
@@ -0,0 +1,21 @@
+import TodoListItem from "./TodoListItem.jsx";
+
+function TodoList({todoList, onCompleteTodo, onUpdateTodo}) {
+
+    const filteredTodoList = todoList.filter(todo => todo.isCompleted === false)
+
+    return filteredTodoList.length === 0 ? (
+        <p>Add todo above to get started</p>
+        ) : (
+            <>
+                <div>
+                    <h2>My Todos</h2>
+                    <ul>
+                        {filteredTodoList.map(todo => <TodoListItem key={todo.id} todo={todo} onCompleteTodo={onCompleteTodo} onUpdateTodo={onUpdateTodo}/>)}
+                    </ul>
+                </div>
+            </>
+        )
+}
+
+export default TodoList
