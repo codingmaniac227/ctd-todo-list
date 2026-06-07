0de99bbfbd69ce49c92290ea3efcebedc3e8af68
Coding <codingmaniac227@gmail.com>
2026-06-07 13:50:00 -0400

    refactor(header): consume authentication state from context

--- src/shared/Header.jsx
+++ src/shared/Header.jsx
@@ -1,7 +1,15 @@
+import {useAuth} from "../contexts/AuthContext.jsx";
+import Logoff from "../features/Logoff.jsx";
+
 export default function Header() {
+    const { isAuthenticated } = useAuth()
+
     return (
         <>
             <h1>Todo List</h1>
+            {isAuthenticated && (
+                <Logoff />
+            )}
         </>
     )
 }
