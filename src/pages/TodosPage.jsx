26767bd4fe6fc500a876b1c19c166e1103dbfbd9
Coding <codingmaniac227@gmail.com>
2026-06-11 16:15:25 -0400

    fix(sorting): fix todo sorting by correcting backend-structured naming convention

--- src/pages/TodosPage.jsx
+++ src/pages/TodosPage.jsx
@@ -50,6 +50,7 @@ export default function TodosPage() {
             type: TODO_ACTIONS.SET_SORT,
             payload: { sortBy: event.target.value }
         })
+        console.log(todoList)
     }
 
     const handleSortDirection = (event) => {
@@ -249,7 +250,7 @@ export default function TodosPage() {
                       throw new Error('Something went wrong')
                   }
                 } catch (err) {
-                    if (debouncedFilterTerm || sortBy !== 'creationDate' || sortDirection !== 'desc') {
+                    if (debouncedFilterTerm || sortBy !== 'createdAt' || sortDirection !== 'desc') {
                         dispatch({
                             type: TODO_ACTIONS.FETCH_ERROR,
                             payload: {
